860a72e4ac316a8210ae09ea03a03f7c64236290
Bob Breznak <143331+bobbrez@users.noreply.github.com>
2024-12-25 12:12:48 -0500

    fix: remove light mode

--- components/Layouts/RootLayout.tsx
+++ components/Layouts/RootLayout.tsx
@@ -15,18 +15,13 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-// export const metadata: Metadata = {
-//   title: "Faunus - Board Management System",
-//   description: "TLM's Board Management System",
-// };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full bg-zinc-900 lg:bg-zinc-950">
+    <html lang="en" className="h-full bg-zinc-900 lg:bg-zinc-950 text-white">
       <body
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-zinc-900 lg:bg-zinc-950`}
       >
